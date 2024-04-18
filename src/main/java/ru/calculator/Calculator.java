78eae9faf8b83a33ba79379f751f61eb255dd194
Iri.Karpuschenko <iren31@mail.ru>
2024-04-18 20:02:03 +0300

    ArgMethod, Calculator

--- src/main/java/ru/calculator/Calculator.java
+++ src/main/java/ru/calculator/Calculator.java
@@ -2,20 +2,15 @@ package ru.calculator;
 
 public class Calculator {
 
+    public static int plus(int first, int second) {
+        int result = first + second;
+        return result;
+    }
+
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int onePlusTwo = one + two;
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        System.out.println(onePlusTwo);
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+        int res = plus(100, 500);
+        System.out.println(res);
     }
 
 }
+
