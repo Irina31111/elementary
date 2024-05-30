30583f89a1a48f68bac7f0b278e3bf0e3a8c36ba
Iri.Karpuschenko <iren31@mail.ru>
2024-05-30 18:08:22 +0300

    4.2 Сумма чисел

---
+++ src/main/java/ru/loop/Counter.java
@@ -0,0 +1,17 @@
+package ru.loop;
+
+public class Counter {
+    public static int sum(int start, int finish) {
+        int total = 0;
+        for (int i = start; i <= finish; i++) {
+            total = total + i;
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(0, 10));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
+    }
+}
