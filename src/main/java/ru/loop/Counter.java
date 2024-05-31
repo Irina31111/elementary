cf2092042c4e2e8ed9c65ae5382fa068a8643e47
Iri.Karpuschenko <iren31@mail.ru>
2024-05-31 13:42:58 +0300

    4.4 Подсчет сумму четных чисел

--- src/main/java/ru/loop/Counter.java
+++ src/main/java/ru/loop/Counter.java
@@ -9,9 +9,23 @@ public class Counter {
         return total;
     }
 
+    public static int sumByEven(int start, int finish) {
+        int total = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0)
+                total = total + i;
+        }
+        return total;
+    }
+
     public static void main(String[] args) {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
+
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 3));
+
     }
 }
---
+++ src/test/java/ru/loop/CounterTest.java
@@ -0,0 +1,23 @@
+package ru.loop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CounterTest {
+    @Test
+    void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromOneToThreeThenTwo() {
+        int result = Counter.sumByEven(1, 3);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
+}
