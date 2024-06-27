f24a076f55085de1fdcf0ba2c000aec83e56409f
Iri.Karpuschenko <iren31@mail.ru>
2024-06-27 20:14:07 +0300

    5

---
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -0,0 +1,9 @@
+package ru.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = {1, 2, 3, 4, 5};
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
