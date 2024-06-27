0f09674c7d06b665ab4779331891770b6dc6ced1
Iri.Karpuschenko <iren31@mail.ru>
2024-06-27 21:16:23 +0300

    5

---
+++ src/main/java/ru/array/Square.java
@@ -0,0 +1,18 @@
+package ru.array;
+
+public class Square {
+    public static int[] calculate(int bound) {
+        int[] ints = new int[bound];
+        for (int i = 0; i  < bound; i++) {
+            ints[i] = i * i;
+        }
+        return ints;
+    }
+
+    public static void main(String[] args) {
+        int[] array = calculate(4);
+        for (int index = 0; index < array.length; index++) {
+            System.out.println(array[index]);
+        }
+    }
+}
