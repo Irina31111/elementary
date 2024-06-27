8f4bff0b9f40459e1749c9f9093267999cb7fbfb
Iri.Karpuschenko <iren31@mail.ru>
2024-06-27 20:53:09 +0300

    5

--- src/main/java/ru/array/ArrayLoop.java
+++ src/main/java/ru/array/ArrayLoop.java
@@ -3,11 +3,11 @@ package ru.array;
 public class ArrayLoop {
     public static void main(String[] args) {
         int[] ints = new int[5];
-        for (int index = 0; index < ints.length; index++) {
+       for (int index = 0; index < ints.length; index++) {
             ints[index] = index * 2 + 3;
-        }
-        for (int index = 0; index < ints.length; index++) {
-            System.out.println(ints[index]);
+       }
+        for (int anInt : ints) {
+            System.out.println(anInt);
         }
     }
 }
