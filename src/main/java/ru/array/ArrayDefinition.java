1cdf05b0e96b03f89602f9e6249faca18ca0fd52
Iri.Karpuschenko <iren31@mail.ru>
2024-06-27 20:44:13 +0300

    5

--- src/main/java/ru/array/ArrayDefinition.java
+++ src/main/java/ru/array/ArrayDefinition.java
@@ -6,5 +6,12 @@ public class ArrayDefinition {
         String[] surnames = new String[100500];
         float[] prices = new float[40];
         System.out.println(ages.length + " " + surnames.length + " " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Max";
+        names[1] = "Ivan";
+        names[2] = "Masha";
+        names[3] = "Ma";
+        System.out.println(names[0] + " " + names[2] + " " + names[3]);
+
     }
 }
