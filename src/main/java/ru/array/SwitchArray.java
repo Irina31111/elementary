1fc4021840261db8a00aa7c81680aa3b21f364e9
Iri.Karpuschenko <iren31@mail.ru>
2024-07-04 21:06:51 +0300

    5.9

--- src/main/java/ru/array/SwitchArray.java
+++ src/main/java/ru/array/SwitchArray.java
@@ -8,11 +8,19 @@ public class SwitchArray {
         return array;
     }
 
+    public static int[] swap(int[] array, int source, int dest) {
+        int temp = array[source];
+        array[source] = array[dest];
+        array[dest] = temp;
+        return array;
+    }
+
     public static void main(String[] args) {
-        int[] nums = new int[] {1, 2, 3, 4, 5, 6};
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6};
         int[] rsl = swapBorder(nums);
         for (int i : rsl) {
             System.out.println(i);
         }
     }
 }
+
