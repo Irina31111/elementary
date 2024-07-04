e674353339d44faa9bf32a0973214df7a6c67cf8
Iri.Karpuschenko <iren31@mail.ru>
2024-07-04 16:23:41 +0300

    5.7 Массивы в тестах

---
+++ src/test/java/ru/array/SquareTest.java
@@ -0,0 +1,17 @@
+package ru.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SquareTest {
+
+    @Test
+    void whenBound5Then014916() {
+        int bound = 5;
+        int[] result = Square.calculate(bound);
+        int[] expected = new int[] {0, 1, 4, 9, 16};
+        assertThat(result).containsExactly(expected);
+    }
+
+}
